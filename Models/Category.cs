4e67bdedb0e0fe4bafcdd17ce1719ce53045bfa7
Jay <tatvajaymodidotnet@gmail.com>
2023-02-10 19:57:29 +0530

    Modified Category model

--- Models/Category.cs
+++ Models/Category.cs
@@ -2,5 +2,13 @@
 {
     public class Category
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public DateTime CreatedDateTime{ get; set; }
+        
     }
 }
